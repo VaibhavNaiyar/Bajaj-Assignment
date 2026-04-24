3109dada1aaad888ddfbd6ce1385659d8fb7f994
VaibhavNaiyar <163887045+VaibhavNaiyar@users.noreply.github.com>
2026-04-24 19:12:59 +0530

    Update BFHLApp.tsx

--- frontend/src/components/BFHLApp.tsx
+++ frontend/src/components/BFHLApp.tsx
@@ -26,7 +26,7 @@ export default function BFHLApp() {
 
     setLoading(true);
     try {
-      const res = await fetch('/bfhl', {
+      const res = await fetch('https://bajaj-assignment-isxf.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ data: items }),
